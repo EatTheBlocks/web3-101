ffd68d2ff102cf970118bf753b43489dfca09cca
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-09-23 15:42:46 -0400

    Added tests for deposit and withdraw

--- section-5/ether-wallet-dapp/test/EtherWallet.js
+++ section-5/ether-wallet-dapp/test/EtherWallet.js
@@ -23,4 +23,54 @@ describe('EtherWallet', function () {
       expect(await etherWallet.owner()).to.equal(owner.address)
     })
   })
+
+  describe('Deposit', function () {
+    it('Should deposit Ether to the contract', async function () {
+      const { etherWallet } = await loadFixture(deployFixture)
+
+      const tx = await etherWallet.deposit({
+        value: ethers.utils.parseEther('1'),
+      })
+      await tx.wait()
+
+      const balance = await ethers.provider.getBalance(etherWallet.address)
+      expect(balance.toString()).to.equal(ethers.utils.parseEther('1'))
+    })
+  })
+
+  describe('Withdraw', function () {
+    it('Should withdraw ether from the contract with zero ETH(Not a very useful test)', async function () {
+      const { etherWallet, owner } = await loadFixture(deployFixture)
+
+      const tx = await etherWallet
+        .connect(owner)
+        .withdraw(owner.address, ethers.utils.parseEther('0'))
+      await tx.wait()
+
+      const balance = await ethers.provider.getBalance(etherWallet.address)
+      expect(balance.toString()).to.equal(ethers.utils.parseEther('0'))
+    })
+
+    it('Should withdraw ether from the contract with non-zero ETH', async function () {
+      const { etherWallet, owner } = await loadFixture(deployFixture)
+
+      // Let's first deposit some Ether to the contract so can withdraw later
+      const depositTx = await etherWallet.deposit({
+        value: ethers.utils.parseEther('1'),
+      })
+      await depositTx.wait()
+      let balance = await ethers.provider.getBalance(etherWallet.address)
+      expect(balance.toString()).to.equal(ethers.utils.parseEther('1'))
+
+      // Let's now execute our withdraw function
+      const withdrawTx = await etherWallet.withdraw(
+        owner.address,
+        ethers.utils.parseEther('1')
+      )
+      await withdrawTx.wait()
+
+      balance = await ethers.provider.getBalance(etherWallet.address)
+      expect(balance.toString()).to.equal(ethers.utils.parseEther('0'))
+    })
+  })
 })
