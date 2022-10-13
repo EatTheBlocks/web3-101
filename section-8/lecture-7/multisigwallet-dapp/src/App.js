a4c9bf758df6b83300af1a05d22c3dc6a9fe9a5f
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-10-13 15:29:58 -0400

    Updated the UI a bit

--- section-8/lecture-7/multisigwallet-dapp/src/App.js
+++ section-8/lecture-7/multisigwallet-dapp/src/App.js
@@ -3,7 +3,11 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Row from 'react-bootstrap/Row'
 import Table from 'react-bootstrap/Table'
 import { useContract, useContractRead, useSigner } from 'wagmi'
 import MultisigWallet from './artifacts/contracts/MultisigWallet.sol/MultisigWallet.json'
@@ -96,95 +100,128 @@ function App() {
         <ConnectButton />
       </div>
 
-      <h3 className='text-5xl font-bold mb-20'>{'Multisig Wallet Info'}</h3>
-      <div>
-        EtherWallet Smart Contract Address:{' '}
-        {multisigWalletContract.addressOrName}
-      </div>
-      <div>EtherWallet Smart Contract Balance: {scBalance} ETH</div>
-      <div>EtherWallet Smart Contract Owners:</div>
-      <ul>
-        {scOwners.map((scOwner, i) => {
-          return <li key={i}>{scOwner}</li>
-        })}
-      </ul>
-      <div>
-        EtherWallet Smart Contract Total Transactions: {scTotalTransactionCount}
-      </div>
+      <Container>
+        <Row>
+          <h3 className='text-5xl font-bold mb-20'>{'Multisig Wallet Info'}</h3>
+        </Row>
+        <Row>
+          <Col md='auto'>Address:</Col>
+          <Col>{multisigWalletContract.addressOrName}</Col>
+        </Row>
+        <Row>
+          <Col md='auto'>Balance:</Col>
+          <Col>{scBalance} ETH</Col>
+        </Row>
+        <Row>
+          <Col md='auto'>Total Withdraw Transactions:</Col>
+          <Col>{scTotalTransactionCount}</Col>
+        </Row>
+        <Row>
+          <Col md='auto'>Owners:</Col>
+          <Col>
+            <ListGroup>
+              {scOwners.map((scOwner, i) => {
+                return <ListGroup.Item key={i}>{scOwner}</ListGroup.Item>
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
 
-      <h3 className='text-5xl font-bold mb-20'>
-        {'Deposit to EtherWallet Smart Contract'}
-      </h3>
-      <Form>
-        <Form.Group className='mb-3' controlId='numberInEthDeposit'>
-          <Form.Control
-            type='text'
-            placeholder='Enter the amount in ETH'
-            onChange={(e) => setEthToUseForDeposit(e.target.value)}
-          />
-          <Button variant='primary' onClick={depositToEtherWalletContract}>
-            Deposit
-          </Button>
-        </Form.Group>
-      </Form>
+      <Container>
+        <Row>
+          <h3 className='text-5xl font-bold mb-20'>
+            {'Deposit to EtherWallet Smart Contract'}
+          </h3>
+        </Row>
+        <Row>
+          <Form>
+            <Form.Group className='mb-3' controlId='numberInEthDeposit'>
+              <Form.Control
+                type='text'
+                placeholder='Enter the amount in ETH'
+                onChange={(e) => setEthToUseForDeposit(e.target.value)}
+              />
+              <Button variant='primary' onClick={depositToEtherWalletContract}>
+                Deposit
+              </Button>
+            </Form.Group>
+          </Form>
+        </Row>
+      </Container>
 
-      <h3 className='text-5xl font-bold mb-20'>
-        {'Withdraw from EtherWallet Smart Contract'}
-      </h3>
-      <Form>
-        <Form.Group className='mb-3' controlId='numberInEthWithdraw'>
-          <Form.Control
-            type='text'
-            placeholder='Enter the amount in ETH'
-            onChange={(e) => setEthToUseForWithdrawal(e.target.value)}
-          />
-          <Form.Control
-            type='text'
-            placeholder='Enter the ETH address to withdraw to'
-            onChange={(e) => setEthAddrToUseForWithdrawal(e.target.value)}
-          />
-          <Button variant='primary' onClick={withdrawFromEtherWalletContract}>
-            Withdraw
-          </Button>
-        </Form.Group>
-      </Form>
+      <Container>
+        <Row>
+          <h3 className='text-5xl font-bold mb-20'>
+            {'Withdraw from EtherWallet Smart Contract'}
+          </h3>
+        </Row>
+        <Row>
+          <Form>
+            <Form.Group className='mb-3' controlId='numberInEthWithdraw'>
+              <Form.Control
+                type='text'
+                placeholder='Enter the amount in ETH'
+                onChange={(e) => setEthToUseForWithdrawal(e.target.value)}
+              />
+              <Form.Control
+                type='text'
+                placeholder='Enter the ETH address to withdraw to'
+                onChange={(e) => setEthAddrToUseForWithdrawal(e.target.value)}
+              />
+              <Button
+                variant='primary'
+                onClick={withdrawFromEtherWalletContract}
+              >
+                Withdraw
+              </Button>
+            </Form.Group>
+          </Form>
+        </Row>
+      </Container>
 
-      <h3 className='text-5xl font-bold mb-20'>
-        {'Pending Withdraw Transactions'}
-      </h3>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Receiver</th>
-            <th>Amount</th>
-            <th>Number of Approvals</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scPendingTransactions.map((tx, i) => {
-            return (
-              <tr key={i}>
-                <td>{i}</td>
-                <td>{tx.to}</td>
-                <td>{tx.amount} ETH</td>
-                <td>{tx.approvals}</td>
-                <td>
-                  <Button
-                    variant='success'
-                    onClick={() =>
-                      approvePendingTransaction(tx.transactionIndex)
-                    }
-                  >
-                    Approve
-                  </Button>
-                </td>
+      <Container>
+        <Row>
+          <h3 className='text-5xl font-bold mb-20'>
+            {'Pending Withdraw Transactions'}
+          </h3>
+        </Row>
+        <Row>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Receiver</th>
+                <th>Amount</th>
+                <th>Number of Approvals</th>
+                <th>Action</th>
               </tr>
-            )
-          })}
-        </tbody>
-      </Table>
+            </thead>
+            <tbody>
+              {scPendingTransactions.map((tx, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{i}</td>
+                    <td>{tx.to}</td>
+                    <td>{tx.amount} ETH</td>
+                    <td>{tx.approvals}</td>
+                    <td>
+                      <Button
+                        variant='success'
+                        onClick={() =>
+                          approvePendingTransaction(tx.transactionIndex)
+                        }
+                      >
+                        Approve
+                      </Button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
     </div>
   )
 }
